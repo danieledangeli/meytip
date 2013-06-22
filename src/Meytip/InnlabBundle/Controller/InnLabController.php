b0ffc9bc4a6d217693d0f93691c136a88764e9a8
erlangb <dangeli88.daniele@gmail.com>
2013-06-22 16:29:24 +0200

    stands

---
+++ src/Meytip/InnlabBundle/Controller/InnLabController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Meytip\InnlabBundle\Controller;
+use FOS\RestBundle\Controller\Annotations\View;
+use JMS\Serializer\SerializationContext;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+class InnLabController extends Controller
+{
+    public function innlabStandsAction()
+    {
+
+        $users_mongo = $this->get('doctrine_mongodb')
+            ->getManager()
+            ->createQueryBuilder('MeytipInnlabBundle:User')
+            ->sort('cash', 'DESC')
+            ->getQuery()
+            ->execute();
+
+        $users = array();
+
+        foreach($users_mongo as $us){
+
+            $us->setCash((int)$us->getCash());
+            $users[] = $us;
+        }
+
+
+        $contest = new SerializationContext();
+        $contest->setGroups(array('login'));
+        $view = \FOS\RestBundle\View\View::create()->
+            setStatusCode(200)->
+            setFormat('json')->
+            setSerializationContext($contest)->
+            setData($users);
+        return $this->get('fos_rest.view_handler')->handle($view);
+
+
+    }
+
+
+
+}
